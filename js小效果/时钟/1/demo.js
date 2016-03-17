133c29e38b559340747328424be65dbe0c2a0e92
ni.li <ni.li@qunar.com>
2016-03-17 17:25:44 +0800

    del

--- js小效果/时钟/1/demo.js
+++
@@ -1,8 +0,0 @@
-// aA[1].onclick = function(){
-    //     iNext++;
-    //     aLi[0].innerHTML = iNow+1;
-    //     aLi[1].innerHTML = iNext+1;
-    //     oUl.style.left = 0;
-    //     iNow = iNext;
-    //     starmove(oUl, 'left', -iWidth)
-    // };
--- 淘宝/js/ie6Fixpng.js
+++
@@ -1,2 +0,0 @@
-// JavaScript Document
-DD_belatedPNG.fix('div,ul,ol,li,dl,dt,dd,h1,h2,h3,h4,h5,h6,p,span,img,a,strong,em,i,input,textarea');
